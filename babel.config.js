0a8037064c66ca922075c40bb7402fac7273859e
Luis Fernando <luisfernandinho094@gmail.com>
2023-01-06 12:10:37 -0300

    feat: setup module resolver

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,21 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          root: ["./"],
+          alias: {
+            "@components": "./src/components",
+            "@screens": "./src/screens",
+            "@utils": "./src/utils",
+            "@services": "./src/services",
+            "@assets": "./assets"
+          },
+        },
+      ],
+    ],
   };
 };
