55f370a1b90c7062e9431871953ad585e550823a
Luis Fernando <luisfernandinho094@gmail.com>
2023-01-06 10:28:13 -0300

    feat: setup tab style

--- App.tsx
+++ App.tsx
@@ -1,12 +1,36 @@
+import { useCallback } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import * as SplashScreen from "expo-splash-screen";
+import {
+  useFonts,
+  OpenSans_400Regular,
+  OpenSans_600SemiBold,
+} from "@expo-google-fonts/open-sans";
 
 import TopTabNavigation from "./src/navigation/TopTabNavigation";
 
+SplashScreen.preventAutoHideAsync();
+
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    OpenSans_400Regular,
+    OpenSans_600SemiBold,
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <SafeAreaProvider>
+    <SafeAreaProvider onLayout={onLayoutRootView}>
       <NavigationContainer>
         <TopTabNavigation />
         <StatusBar style="auto" />
--- src/navigation/TopTabNavigation.tsx
+++ src/navigation/TopTabNavigation.tsx
@@ -1,4 +1,5 @@
 import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
+import { Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 import Control from "../screens/Control";
@@ -10,10 +11,45 @@ const Tab = createMaterialTopTabNavigator();
 export default function TopTabNavigation() {
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <Tab.Navigator>
-        <Tab.Screen name="monitoring" component={Monitoring} />
-        <Tab.Screen name="control" component={Control} />
-        <Tab.Screen name="history" component={History} />
+      <Text
+        style={{
+          fontFamily: "OpenSans_600SemiBold",
+          fontSize: 18,
+          fontWeight: "600",
+          marginLeft: 14,
+          marginTop: 12,
+          marginBottom: 8,
+        }}
+      >
+        Chocadeira
+      </Text>
+      <Tab.Navigator
+        screenOptions={{
+          tabBarLabelStyle: {
+            fontFamily: "OpenSans_600SemiBold",
+            fontSize: 14,
+            textTransform: "capitalize",
+          },
+          tabBarIndicatorStyle: {
+            backgroundColor: "#4E9DEC",
+          },
+        }}
+      >
+        <Tab.Screen
+          name="monitoring"
+          component={Monitoring}
+          options={{ tabBarLabel: "Monitoramento" }}
+        />
+        <Tab.Screen
+          name="control"
+          component={Control}
+          options={{ tabBarLabel: "Controle" }}
+        />
+        <Tab.Screen
+          name="history"
+          component={History}
+          options={{ tabBarLabel: "Histórico" }}
+        />
       </Tab.Navigator>
     </SafeAreaView>
   );
