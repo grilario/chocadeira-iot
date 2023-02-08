c08a14fde5baf553ea73620962d4b7a8a316f898
Luis Fernando <luisfernandinho094@gmail.com>
2023-02-08 17:11:06 -0300

    feat: setup esp8266 project

---
+++ esp8266/src/main.cpp
@@ -0,0 +1,15 @@
+#include <Arduino.h>
+#include <Adafruit_Sensor.h>
+#include <ESP8266WiFi.h>
+#include <FirebaseESP8266.h>
+#include <DHT.h>
+
+void setup()
+{
+  // put your setup code here, to run once:
+}
+
+void loop()
+{
+  // put your main code here, to run repeatedly:
+}
